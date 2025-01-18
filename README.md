09e7f2c478ae853b6da00181911510f3f0b065a1
Blue Zephyr <blue.zephyr.git@gmail.com>
2025-01-18 16:02:20 +0100

    Control a LED with a button

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Experiments with the [Embassy](https://embassy.dev/) framework on the Raspberry Pi Pico.
 
+## Setup
+
+The board has a LED connected to GP22 and a button connected to GP0. When the button is
+pressed, the LED will toggle. The implementation uses two Embassy tasks, one for the
+toggling of the LED and one for the button handling. The communication between the tasks
+is done using a channel. The button task also has a `count` variable. Each time the button
+is pressed, the `count` variable is incremented. The value is sent to the LED task and is
+printed when the LED is toggled.
+
 ## Links
 
 * [Embassy](https://embassy.dev/)
--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,48 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::gpio::{Level, Output};
-use embassy_time::Timer;
+use embassy_rp::gpio::{Input, Level, Output, Pull};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::Channel;
+use embassy_time::Duration;
 use {defmt_rtt as _, panic_probe as _};
 
+// Define a channel to communicate between the tasks
+static CHANNEL: Channel<ThreadModeRawMutex, u32, 1> = Channel::new();
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     info!("Hello World!");
 
+    // Init peripherals
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_22, Level::Low);
+    let button = Input::new(p.PIN_0, Pull::Up);
+    let led = Output::new(p.PIN_22, Level::Low);
 
+    // Spawn the tasks
+    spawner.spawn(button_task(button)).unwrap();
+    spawner.spawn(led_task(led)).unwrap();
+}
+
+// Button task
+#[embassy_executor::task]
+async fn button_task(mut button: Input<'static>) {
+    let mut count: u32 = 0;
     loop {
-        info!("LED On");
-        led.set_high();
-        Timer::after_millis(500).await;
+        button.wait_for_high().await;
+        button.wait_for_low().await;
+        count += 1;
+        CHANNEL.send(count).await;
+        embassy_time::Timer::after(Duration::from_millis(100)).await;
+    }
+}
 
-        info!("LED Off");
-        led.set_low();
-        Timer::after_millis(500).await;
+// LED task
+#[embassy_executor::task]
+async fn led_task(mut led: Output<'static>) {
+    loop {
+        let count = CHANNEL.receive().await;
+        info!("Button presses: {}", count);
+        led.toggle();
     }
 }
